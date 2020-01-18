9976a9ed3da98c344750ed1b4b101f0cb6ba7ea1
mattmega4 <mattmega4@gmail.com>
2020-01-17 19:01:16 -0800

    Initial Commit

---
+++ Listus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Listus
+//
+//  Created by Matthew Howes Singleton on 1/17/20.
+//  Copyright © 2020 Matthew Howes Singleton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
